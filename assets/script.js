355a68b971c43271af7836b6747a89b03ab67dee
Alap Raval <alapsraval@gmail.com>
2021-05-06 02:17:41 -0500

    finished UI

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ $(function () {
 
     let hours = [];
     let currentDayEl = $('#currentDay');
+    let timeBlocksEl = $('#timeBlocks');
 
     function init() {
         for (var i = 9; i <= 21; i++) {
@@ -14,46 +15,53 @@ $(function () {
             displayCurrentDay();
         }, 1000);
 
-        //color rows based on time of day
-        //use interval to update row color
+        addRows();
+
+        //use interval to update row color every 10 mins
+        setRowColor();
         setInterval(function () {
             setRowColor();
-        }, 1000);
-        
+        }, 60000);
+
     }
 
     function addRows() {
-
+        $.each(hours, function (index, hour) {
+            let hourInLT = moment(hour, "h").format('LT');
+            let timeBlock = $('<div>')
+                .addClass('row time-block')
+                .data('hour', hour);
+            let hourCol = $('<div>').addClass('col-md-1 hour pt-2 small').text(`${hourInLT}`);
+            let textCol = $('<textarea>').addClass('col-md-10 description');
+            let btnCol = $('<button>').addClass('col-md-1 saveBtn').append('<i class="fas fa-save"></i>');
+            timeBlock.append(hourCol, textCol, btnCol);
+            timeBlocksEl.append(timeBlock);
+        });
     }
 
+    //color rows based on time of day
     function setRowColor() {
         //set variable to current hour
-        let currentHour = 12;//moment().hour();
+        let currentHour = moment().hour();
         //loop over each row of the class (time-block)
         $('.time-block').each(function () {
+            //find row time using data attribute add appropriate class
             let rowHour = $(this).data('hour');
             if (rowHour < currentHour) {
                 //if rowHour is in past, add class "past" and make field read only
-                $(this).children('textarea').addClass('past').attr('readonly',true);
+                $(this).children('textarea').addClass('past').attr('readonly', true);
             } else if (rowHour == currentHour) {
                 //if rowHour is current
-                $(this).children('textarea').addClass('current');
+                $(this).children('textarea').addClass('present');
             } else {
                 //if rowHour is in future
                 $(this).children('textarea').addClass('future');
             }
         })
 
-        //find row time using data attribute (9 < 21)
-        //add the past class to $(this).addClass('past)
     }
 
-    //color rows based on time of day
-    //use interval to update row color
-
-
     // display current day on the page
-
     function displayCurrentDay() {
         let currentDate = moment().format('MMM DD, YYYY hh:mm:ss');
         currentDayEl.text(currentDate);
